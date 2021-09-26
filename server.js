58f51e71538d3f83643be56cacadb5ae2b696104
Alexander Islip <43761702+IslipAI@users.noreply.github.com>
2021-09-26 17:13:26 -0400

    Axios Installed.

--- server.js
+++ server.js
@@ -17,10 +17,11 @@ app.use(bodyParser.json())
 //Initialize fresh table upon api startup.
 db.serialize(function(){
     db.run("DROP TABLE IF EXISTS Movies");
-    db.run('CREATE TABLE Movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, release_year TEXT, time_viewed DATETIME)');
+    db.run('CREATE TABLE Movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, release_year TEXT, time_viewed TEXT)');
 });
 
 //Collection API Routers
+//GET COLLECTION
 app.get("/api/", function(req, res){
     console.log("GET Request!");
 
@@ -34,20 +35,26 @@ app.get("/api/", function(req, res){
     });
 });
 
+//PUT COLLECTION
 app.put('/api/', function(req, res){
     console.log('PUT Request!')
     res.send("Collection PUT REQUEST");
 });
 
+//POST COLLECTION
 app.post('/api/', function(req, res){
     console.log('POST Request!')
     console.log(req.body)
 
-    db.run("INSERT INTO Movies (title, release_year, time_viewed) VALUES (?,?,?)", [req.body.title, req.body.releaseyear, new Date()]);
+    var date = new Date();
+
+    sql = "INSERT INTO Movies (title, release_year, time_viewed) VALUES (?,?,?)";
+    db.run(sql, [req.body.title, req.body.releaseyear, date.toString()]);
     
     res.send("Collection POST REQUEST");
 });
 
+//DELETE COLLECTION
 app.delete('/api/', function(req, res){
     console.log('DELETE Request!')
     db.run('DELETE * FROM Movies', function(error){
@@ -62,6 +69,7 @@ app.delete('/api/', function(req, res){
 
 
 //Item API Routers
+//GET ITEM
 app.get('/api/:id', function(req, res){
     console.log('GET Request!')
     sql = 'SELECT * FROM Movies WHERE id = ?';
@@ -75,11 +83,21 @@ app.get('/api/:id', function(req, res){
     });
 });
 
+//PUT ITEM
 app.put('/api/:id', function(req, res){
     console.log('PUT Request!')
-    res.send("Item PUT REQUEST");
+    sql = 'UPDATE Movies SET title = ?, release_year = ?, time_viewed = ? WHERE id = ?';
+
+    db.run(sql, [req.body.title, req.body.releaseyear, new Date().toString(), req.params.id], function(error){
+        if(error){
+            console.log(error);
+        }else{
+            res.send("UPDATE ITEM WAS SUCCESSFUL")
+        }
+    });
 });
 
+//DELETE ITEM
 app.delete('/api/:id', function(req, res){
     console.log('DELETE Request!')
     db.run('DELETE FROM Movies WHERE id = ?', [req.params.id], function(error){
@@ -90,6 +108,7 @@ app.delete('/api/:id', function(req, res){
         }
     });
 });
+
 
 
 const server = app.listen(3000, function(){
